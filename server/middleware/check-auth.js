f825dac2add31094a66a2e14ca83d7c364d7cd00
Ori Silberman <orisil1809@gmail.com>
2024-08-10 18:05:09 +0300

    Added authentication

---
+++ server/middleware/check-auth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const HttpError = require("../models/http-error");
+module.exports = (req, res, next) => {
+  if (req.method === "OPTIONS") {
+    // Before a POST request (for example), the browser sends an OPTIONS request to check if the POST request will be permitted. This OPTIONS request doesn't contain a token and it doesn't need to. So we allow it to proceed.
+    return next();
+  }
+  try {
+    const token = req.headers.authorization.split(" ")[1]; // Authorization: 'Bearer TOKEN'
+    if (!token) {
+      throw new HttpError("Authentication failed!", 401); // Will make us reach the catch block here
+    }
+    const decodedToken = jwt.verify(token, "supersecret_dont_share");
+    req.userData = { userId: decodedToken.userId };
+    next();
+  } catch (err) {
+    const error = new HttpError("Authentication failed!", 401);
+    return next(error);
+  }
+};
