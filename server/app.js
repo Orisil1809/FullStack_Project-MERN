88ded090f840575a4d0d0c4c77adb626a2453f59
Ori Silberman <orisil1809@gmail.com>
2024-08-11 13:28:35 +0300

    Changed nodemon to node command and used prod port used by Render

--- server/app.js
+++ server/app.js
@@ -49,7 +49,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1kdsmcb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
   )
   .then(() => {
-    app.listen(5001);
+    app.listen(process.env.PORT || 5001);
   })
   .catch((err) => {
     console.log(err);
